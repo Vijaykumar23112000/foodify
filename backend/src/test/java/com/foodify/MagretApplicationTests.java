995b7c557eb14b353a0081f29ed890e101355950
Vijaykumar23112000 <mathew5359302@gmail.com>
2024-05-16 21:36:19 +0530

    Day-1 : Foodify -> Initialized The Project : Test Run

---
+++ backend/src/test/java/com/foodify/MagretApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagretApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
