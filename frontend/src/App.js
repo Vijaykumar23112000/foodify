cb849de1f51c56f97a4a5a84d1203516a409517e
Vijaykumar23112000 <mathew5359302@gmail.com>
2024-05-25 21:11:13 +0530

    Day-7 : Foodify -> Frontend Implementation

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { darkTheme } from './theme/DarkTheme';
+
+function App() {
+  return (
+
+    <div>Hello World</div>
+
+  );
+}
+
+export default App;
